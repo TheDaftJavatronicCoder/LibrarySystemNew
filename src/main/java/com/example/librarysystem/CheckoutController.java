a66d0834b5769182dbaddf1ec9f3032c1da32af2
rubenurman <ruburm-2@student.ltu.se>
2023-05-17 14:16:15 +0200

    hejhej

--- src/main/java/com/example/librarysystem/CheckoutController.java
+++ src/main/java/com/example/librarysystem/CheckoutController.java
@@ -1,4 +1,43 @@
 package com.example.librarysystem;
 
-public class CheckoutController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class CheckoutController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    @FXML
+    private Button startPageButton5;
+
+
+    public void switchToStartPage5() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("startpage.fxml"));
+        Parent myPagesParent = fxmlLoader.load();
+        Scene myPagesScene = new Scene(myPagesParent);
+        Stage currentStage = (Stage) startPageButton5.getScene().getWindow();
+        currentStage.setScene(myPagesScene);
+
+
+    }
+
 }
